f3860c2c7c1bc2c4ad9ea4f42ed9df2720fc188a
migggg <miggireniva123@gmail.com>
2026-06-24 20:57:02 +0800

    add docker files

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Linking storage..."
+php artisan storage:link
+
+echo "Running migrations..."
+php artisan migrate --force
