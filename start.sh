517065b2d953ae829a6f074e47794ede8b4c1c20
migggg <miggireniva123@gmail.com>
2026-06-24 21:32:59 +0800

    fix startup script

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+php-fpm -D
+sleep 2
+
 php artisan config:cache
 php artisan route:cache
 php artisan storage:link
 php artisan migrate --force
 
-php-fpm -D
 nginx -g 'daemon off;'
